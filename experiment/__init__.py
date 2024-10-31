b8dea2dc7e52ab7b7a154431f514f3fe1d92161a
Erik Sarrazin <sarrazin@uni-mainz.de>
2024-10-31 15:48:02 +0100

    31-10-24

--- experiment/__init__.py
+++ experiment/__init__.py
@@ -6,6 +6,7 @@ import random
 import numpy as np
 import scipy.spatial.distance
 from concurrent.futures import ThreadPoolExecutor
+import fasttext
 
 from settings import PARTICIPANT_FIELDS
 
@@ -368,26 +369,57 @@ class Player(BasePlayer):
     guesser8 = models.IntegerField(choices=[[2, 'stimme vollkommen zu'], [1, 'stimme zu'], [0, 'neutral'], [-1, 'stimme nicht zu'], [-2, 'stimme überhaupt nicht zu']], label='<b>8. </b>Der Wettkampf mit den anderen Ratenden hat mich sehr motiviert.', widget=widgets.RadioSelectHorizontal, blank=True)
     guesser9 = models.IntegerField(choices=[[2, 'stimme vollkommen zu'], [1, 'stimme zu'], [0, 'neutral'], [-1, 'stimme nicht zu'], [-2, 'stimme überhaupt nicht zu']], label='<b>9. </b>Die Bonuszahlung hat mich sehr motiviert.', widget=widgets.RadioSelectHorizontal, blank=True)
 
-class Model:
-    def __init__(player, model="vectors_german.txt.gz", dictionary="vocab_german.txt", pattern="^[a-z][a-z-]*[a-z]$"):
-        player.model_file = model
-        player.dictionary_file = dictionary
-        player.pattern = re.compile(pattern)
-        player.words = set()
+# class Model:
+#     def __init__(player, model="vectors_german.txt.gz", dictionary="vocab_german.txt", pattern="^[a-z][a-z-]*[a-z]$"):
+#         player.model_file = model
+#         player.dictionary_file = dictionary
+#         player.pattern = re.compile(pattern)
+#         player.words = set()
 
-    def load_words(player):
-        with open(player.dictionary_file, "r", encoding="utf-8") as f:
-            player.words.update(line.strip() for line in f if player.pattern.match(line))
+#     def load_words(player):
+#         with open(player.dictionary_file, "r", encoding="utf-8") as f:
+#             player.words.update(line.strip() for line in f if player.pattern.match(line))
+
+#     def get_vector(player, word):
+#         with gzip.open(player.model_file, "rt", encoding="utf-8") as f:
+#             for line in f:
+#                 tokens = line.split(" ")
+#                 if tokens[0] == word:
+#                     return np.array(tokens[1:], dtype=np.float32)
+#         return None
+
+#     def distance(player, word1, word2):
+#         vector1 = player.get_vector(word1)
+#         vector2 = player.get_vector(word2)
+#         if vector1 is not None and vector2 is not None:
+#             return scipy.spatial.distance.cosine(vector1, vector2) * 100
+#         return None
+    
+#     def calculate_originality(player, word_pair, mystery_word):
+#         dist_1 = player.distance(word_pair.split(" + ")[0], mystery_word)
+#         dist_2 = player.distance(word_pair.split(" + ")[1], mystery_word)
+#         if dist_1 is not None and dist_2 is not None:
+#             return (dist_1 + dist_2) / 2
+#         return None
+    
+#     def __enter__(player):
+#         player.load_words()
+#         return player
+
+#     def __exit__(player, exc_type, exc_value, traceback):
+#         pass
+
+class Model:
+    def __init__(player, model='cc.de.300.bin'):
+        player.model_file = model
+        print(f'Loading FastText model from {player.model_file}...')
+        player.model = fasttext.load_model(player.model_file)
+        print('Model loaded successfully.')
 
     def get_vector(player, word):
-        with gzip.open(player.model_file, "rt", encoding="utf-8") as f:
-            for line in f:
-                tokens = line.split(" ")
-                if tokens[0] == word:
-                    return np.array(tokens[1:], dtype=np.float32)
-        return None
-
-    def distance(player, word1, word2):
+        return player.model.get_word_vector(word)
+    
+    def distance(player, word1: str, word2: str):
         vector1 = player.get_vector(word1)
         vector2 = player.get_vector(word2)
         if vector1 is not None and vector2 is not None:
@@ -400,12 +432,11 @@ class Model:
         if dist_1 is not None and dist_2 is not None:
             return (dist_1 + dist_2) / 2
         return None
-    
+
     def __enter__(player):
-        player.load_words()
         return player
 
-    def __exit__(player, exc_type, exc_value, traceback):
+    def __exit__(self, exc_type, exc_value, traceback):
         pass
 
 def creating_session(subsession: Subsession):
